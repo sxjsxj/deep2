7d1ef2e3c45cf084986c66a3d6e12f684de6b45a
sunxijin <sunxijin@sunxijin-pc>
2017-04-09 00:58:34 +0800

    投资方bug

---
+++ src/main/java/com/deep/two/util/AttachConstant.java
@@ -0,0 +1,7 @@
+package com.deep.two.util;
+
+public final class AttachConstant {
+	public static final String LOGOFILE="logoFile";
+	public static final String LEADERFILE="LEADERFILE";
+	public static final String ATTACHFILE="ATTACHFILE";
+}
